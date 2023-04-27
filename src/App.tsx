e5c23354cdf2e531fb4cf52c00ccabd225b79c4d
Irving Juárez <irvingjuarez274@gmail.com>
2023-04-27 12:23:48 -0600

    [ADD] useQuery hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import SearchBar from './components/SearchBar'
+import useQuery from './hooks/useQuery'
 import './App.css'
 
 // import viteLogo from '/vite.svg'
---
+++ src/hooks/useQuery/index.ts
@@ -0,0 +1,20 @@
+import Commerces from "@app/controllers/commerces"
+import { useState } from "react"
+
+const useQuery = (...props: string[]) => {
+    const commerces = new Commerces(...props)
+    const dataProps = commerces.allProps()
+    const [currentQuery, useCurrentQuery] = useState(commerces.createQuery())
+
+    const createQuery = () => {
+        useCurrentQuery(commerces.createQuery())
+    }
+
+    return {
+        createQuery,
+        dataProps,
+        query: currentQuery
+    }
+}
+
+export default useQuery
