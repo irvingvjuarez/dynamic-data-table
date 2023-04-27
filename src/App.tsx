ff44ee1d8bfbc1b5adec3e920a83dc880c710c05
Irving Juárez <irvingjuarez274@gmail.com>
2023-04-26 18:25:45 -0600

    [ADD] frontend structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const { query, createQuery } = useQuery()
 
   return (
     <>
-      <form>
+      <SearchBar onSearch={createQuery} />
+      <DataTable />
+      <QueryDisplayer content={query} />
+
+      {/* SearchBar component 👇 */}
+      {/* <form>
         <input type="search" placeholder="Search by ID, CUIT or Name" />
         <button type="button">
           Search
         </button>
-      </form>
+      </form> */}
     </>
   )
 }
